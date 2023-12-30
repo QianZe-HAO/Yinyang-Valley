f5b68c72489f1a2009a25d799022fe19b2aebfff
Qian Zehao <qianzehao123@gmail.com>
2023-12-30 23:22:31 +0000

    update ChatBot Page

--- ChatBot.py
+++ ChatBot.py
@@ -1,13 +1,17 @@
 from openai import OpenAI
 import streamlit as st
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+st.set_page_config(layout="wide")
 
-st.title("💬 Chatbot")
+# with st.sidebar:
+#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+st.sidebar.markdown("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
+
+
+st.title("💬 Chatbot with Knowledge and Search")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
